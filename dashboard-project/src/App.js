afb4259e8a4ff869161006d4752a45d7b98fc588
Frankenstei <enwerefrankenstein@gmail.com>
2023-10-02 14:24:24 +0000

    dashboard-app frontend without any functionality

--- dashboard-project/src/App.js
+++ dashboard-project/src/App.js
@@ -4,15 +4,17 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-import Contact from "./scenes/contacts";
-import Invoice from "./scenes/invoices";
-import Geography from "./scenes/geography";
-import Line from "./scenes/line";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
 import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme"
-
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -29,18 +31,21 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contact />} />
-              <Route path="/invoices" element={<Invoice />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/line" element={<Line />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
-      </ThemeProvider> 
+      </ThemeProvider>
     </ColorModeContext.Provider>
-  )
+  );
 }
 
 export default App;
--- dashboard-project/src/scenes/contacts/index.jsx
+++ dashboard-project/src/scenes/contacts/index.jsx
@@ -1,77 +1,103 @@
-import Header from "../../components/Header";
-import { Box, Typography, useTheme } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataContacts } from "../../data/mockData";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
 
-const Contact = () => {
+const Contacts = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   const columns = [
-    { field: 'id', headerName: 'ID', flex: 0.5 },
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "registrarId", headerName: "Registrar ID" },
     {
-        field: 'registrarId',
-        headerName: 'Register ID',
-        type: "number",
-        flex: 1,
-      },
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
     {
-      field: 'name',
-      headerName: 'Name',
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
       flex: 1,
     },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-    },
-    {
-      field: 'phone',
-      headerName: 'Phone Number',
+      field: "email",
+      headerName: "Email",
       flex: 1,
     },
     {
-      field: 'email',
-      headerName: 'Email',
+      field: "address",
+      headerName: "Address",
       flex: 1,
     },
     {
-      field: 'address',
-      headerName: 'Address',
+      field: "city",
+      headerName: "City",
       flex: 1,
     },
     {
-        field: 'city',
-        headerName: 'City',
-        flex: 1,
-      },
-    {
-        field: 'zipCode',
-        headerName: 'Zip Code',
-        flex: 1,
-    }
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+    },
   ];
 
   return (
-      <Box sx={{ height: 400, width: '100%' }}>
-        <Header title = "CONTACTS" subtitle="List of Contacts for Future Refrence" />
+    <Box m="20px">
+      <Header
+        title="CONTACTS"
+        subtitle="List of Contacts for Future Reference"
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
         <DataGrid
           rows={mockDataContacts}
           columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
-              },
-            },
-          }}
-          pageSizeOptions={[5]}
-          checkboxSelection
-          disableRowSelectionOnClick
+          components={{ Toolbar: GridToolbar }}
         />
       </Box>
-    );
-}
+    </Box>
+  );
+};
 
-export default Contact;
+export default Contacts;
